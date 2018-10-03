54d51af0664d52149166ca6783c185d1f46f869e
damunza <danidamunza@gmail.com>
2018-10-03 13:51:06 +0300

    added the requirements

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
 # Create your models here.
+# class Profile(models.Model):
+#     name = models.ForeignKey(User,on_delete=models.CASCADE)
+#     profile_photo = models.ImageField(upload_to ='images/')
+#     bio = models.TextField()
