412be28b21231ea8267638f52d1fbd1df9cb6fa2
damunza <danidamunza@gmail.com>
2018-10-03 13:51:46 +0300

    created the profile model

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from tinymce.models import HTMLField
 
 # Create your models here.
-# class Profile(models.Model):
-#     name = models.ForeignKey(User,on_delete=models.CASCADE)
-#     profile_photo = models.ImageField(upload_to ='images/')
-#     bio = models.TextField()
+class Profile(models.Model):
+    name = models.ForeignKey(User,on_delete=models.CASCADE)
+    profile_photo = models.ImageField(upload_to ='images/')
+    bio = models.TextField()
